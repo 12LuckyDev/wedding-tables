583eecba9592fc05c92d1392c43502513fb555c0
12LuckyDev <12luckydev@gmail.com>
2025-06-08 15:25:52 +0200

    Assign groups to existing guest on import

--- src/core/stores/wedding-store-methods/import-guest.ts
+++ src/core/stores/wedding-store-methods/import-guest.ts
@@ -1,10 +1,20 @@
-import { mappify } from '@12luckydev/utils';
-import { GroupImportType, GuestImportSummaryModel, Wedding } from '../../models';
+import { GroupImportType, Guest, GuestImportSummaryModel, Wedding } from '../../models';
 import { buildColor } from '../../helpers';
+
+const assingExistingGuestsToGroup = (guestMap: Map<string, Guest>, groupId: string, possibleGroupsGuests: Guest[]) => {
+  possibleGroupsGuests.forEach(({ id }) => {
+    const guest = guestMap.get(id);
+    if (!guest) {
+      return;
+    }
+    guestMap.set(id, { ...guest, groupId, color: buildColor(groupId) });
+  });
+};
 
 export const importGuest = (oldState: Wedding, { groups, newSingleGuests }: GuestImportSummaryModel): Wedding => {
   const { _allGuests: oldAllGuest, guestIds: oldGuestIds } = oldState;
   const newGuest = [...newSingleGuests];
+  const newAllGuests = new Map(oldAllGuest);
 
   groups.forEach((group) => {
     switch (group.type) {
@@ -12,6 +22,7 @@ export const importGuest = (oldState: Wedding, { groups, newSingleGuests }: Gues
       case GroupImportType.existingGroup: //TODO add group to existing guests
         const [groupId] = group.groupIds;
         group.newGuests.forEach((g) => newGuest.push({ ...g, groupId, color: buildColor(groupId) }));
+        assingExistingGuestsToGroup(newAllGuests, groupId, group.existingGuests);
         break;
       case GroupImportType.manyGroups:
         //TODO
@@ -19,7 +30,7 @@ export const importGuest = (oldState: Wedding, { groups, newSingleGuests }: Gues
     }
   });
 
-  const allGuests = mappify([...oldAllGuest.values(), ...newGuest], 'id');
+  newGuest.forEach((guest) => newAllGuests.set(guest.id, guest));
   const guestIds = [...oldGuestIds, ...newGuest.map(({ id }) => id)];
-  return { ...oldState, guestIds, _allGuests: allGuests };
+  return { ...oldState, guestIds, _allGuests: newAllGuests };
 };
