9b568c97e6f92d38d2564ba0ff1a062058a11f9b
12LuckyDev <12luckydev@gmail.com>
2025-06-23 22:15:09 +0200

    calcFontContrast fix

--- src/core/helpers/colors/calc-font-contrast.ts
+++ src/core/helpers/colors/calc-font-contrast.ts
@@ -6,8 +6,8 @@ export const calcFontContrast = (hexcolor: string): string => {
   const cleanHex = hexcolor.replace('#', '');
 
   const r = parseInt(cleanHex.slice(0, 2), 16);
-  const g = parseInt(cleanHex.slice(2, 2), 16);
-  const b = parseInt(cleanHex.slice(4, 2), 16);
+  const g = parseInt(cleanHex.slice(2, 4), 16);
+  const b = parseInt(cleanHex.slice(4, 6), 16);
 
   const yiq = (r * 299 + g * 587 + b * 114) / 1000;
 
